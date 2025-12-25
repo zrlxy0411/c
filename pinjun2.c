ed96a38e35f5eb33cdc25062144a31ea7657bf0a
zrlxy0411 <3205925683@qq.com>
2025-12-25 20:41:57 +0800

    Revise main function for better clarity and functionality
    Refactor main function and improve input handling.

--- pinjun2.c
+++ pinjun2.c
@@ -1,22 +1,19 @@
-```c
 #include <stdio.h>
-```
-int main（）[
-int a = 0;
-int b = 0;
-int c = 0;
-int number = 0;
-int count = 0;  // 整型变量 count 赋值为 0 。
-printf（“请输入你的值”）
-do[
-int a = 0;  // 假设此处省略了
-int b =
-数字+=a；
-计数++；
-按键移动焦点。或者，使用
-int number =
-]
-// 假设此处为完整的语
-其他的0; 而做
-做
-]
+int main(){
+    int a = 0;
+    int b = 0;
+    int number = 0;
+    int count = 0;
+    printf("请输入你的值\n");
+    do{ scanf ("%d",&a);
+        if(a != -1){       //注意if判断条件和if在程序中的位置
+        number += a;
+        count ++;
+    }
+    }
+    while(a != -1);
+    printf("你所求的平均值是：%f",(float)number/count);
+    return 0;
+
+    
+}
